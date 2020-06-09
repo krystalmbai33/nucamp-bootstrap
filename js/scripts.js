679fa35edff55d41e82b448f5b90420a0333e115
Krystal Bailey <krystalmbai33@gmail.com>
2020-06-08 23:53:28 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+});
 
+$(function() {
     $("#reserveButton").click(function(){
         $('#reserveModal').modal('show');
         }); 
 
     $("#loginButton").click(function(){
         $('#loginModal').modal('show');
-        }); 
-    
-
+        });  
 });
